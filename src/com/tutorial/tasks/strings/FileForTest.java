6400c7360c38aa8b5a445bb990b08a93aa86f687
Ddzhurko <Denis.dzhurko@gmail.com>
2018-09-24 22:26:57 +0300

    Deleted unuse class

--- src/com/tutorial/tasks/strings/FileForTest.java
+++
@@ -1,9 +0,0 @@
-package com.tutorial.tasks.strings;
-
-import java.io.File;
-
-public class FileForTest {
-
-    File file = new File("D:\\JavaProjects\\labs\\src\\com\\tutorial\\tasks\\strings" +
-            "\\ForTestingOfStrings.txt");
-}
