830a5ae49f9a62acb54c806b03b0b0a17ef068d6
Ddzhurko <Denis.dzhurko@gmail.com>
2018-09-28 22:48:19 +0300

    Update XmlParserDom

--- src/com/tutorial/projects/xml/parsers/XmlParserDom.java
+++ src/com/tutorial/projects/xml/parsers/XmlParserDom.java
@@ -1,10 +1,6 @@
 package com.tutorial.projects.xml.parsers;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -13,36 +9,54 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 
 public class XmlParserDom {
+    private static void printNote(NodeList nodeList) {
+
+        for (int count = 0; count < nodeList.getLength(); count++) {
+            Node tempNode = nodeList.item(count);
+
+            //check for node
+
+            if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                //get node and value
+
+                System.out.println("\nNode name = " + tempNode.getNodeName() + " [OPEN]");
+                System.out.println("Node value = " + tempNode.getTextContent());
+
+                if (tempNode.hasAttributes()) {
+
+                    //get attributes names and values
+
+                    NamedNodeMap nodeMap = tempNode.getAttributes();
+                    for (int i = 0; i < nodeMap.getLength(); i++) {
+                        Node node = nodeMap.item(i);
+                        System.out.println("Attribute name = " + node.getNodeName());
+                        System.out.println("Attribute value = " + node.getNodeValue());
+                    }
+                }
+                if (tempNode.hasChildNodes()) {
+
+                    //searching for nodes
+                    printNote(tempNode.getChildNodes());
+                }
+                System.out.println("Node name = " + tempNode.getNodeName() + " [ CLOSE]");
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         try {
             File xmlFile = new File(
-                    "D:\\JavaProjects\\GitHub\\OOP\\src\\com\\tutorial\\projects\\xml\\Tariffs.xml");
+                    "D:\\JavaProjects\\GitHub\\Java\\src\\com\\tutorial\\projects\\xml\\Tariffs.xml");
 
 //String line = reader.readLine();
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-//    InputSource is = new InputSource(new StringReader(line));
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             Document doc = builder.parse(xmlFile);
-            doc.getDocumentElement().normalize();
             System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
-            NodeList nList = doc.getElementsByTagName("name");
-            System.out.println("-------------------------------------");
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-                Node nNode = nList.item(temp);
-                System.out.println("\nCurrent element " + nNode.getNodeName() + ": " + nNode.getNodeValue());
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    System.out.println("name: " + eElement.getAttribute("name"));
-                    System.out.println("payroll: " + eElement.getElementsByTagName("payroll").item(0).getTextContent());
-
-                    System.out.println();
-
-                }
+            if (doc.hasChildNodes()) {
+                printNote(doc.getChildNodes());
             }
-//            NodeList nList = document.getElementsByTagName("tariff");
-//            Node namedItem = nList.item(0).getAttributes().getNamedItem("operatorname");
-//            System.out.println(namedItem.getNodeValue());
         } catch (ParserConfigurationException pce) {
             pce.printStackTrace();
         } catch (SAXException se) {
