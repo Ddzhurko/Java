6df7b95d90e676e04d7758c37ad61e39546e7f5c
Ddzhurko <Denis.dzhurko@gmail.com>
2018-10-09 22:54:15 +0300

    Correct regular fof sentence

--- src/com/tutorial/projects/strings/Demo.java
+++ src/com/tutorial/projects/strings/Demo.java
@@ -6,99 +6,130 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Demo {
 
-    private static final String SENTENСE_REGEX = "(?=[\\.\\!\\?])"; //(?=[\.\!\?]);
-    private static final String WORD_REGEX = "\\s"; //[\.; \?! \—\,];
+    private static final String SENTENСE_REGEX = "[.|?|!|;|-|!-]";//"[.?!-;]+;";
+    private static final String WORD_REGEX = "[\\s|.|?|!|;|-|!-]"; //[\.; \?! \—\,];
     private static final String SYMBOL_REGEX = "(?!= [|\\s|,\\s|.|\\f])";
 
-//    private static final String SIGN_REGEX = "[.;?!—,]";
+    public static void main(String[] args) throws IOException {
+        List<Sentence> sentences = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(
+                "D:\\JavaProjects\\GitHub\\Java\\src\\com\\tutorial\\projects\\strings\\test.txt"
+        ))) {
 
-    public static void main(String[] args) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(
-                    "D:\\JavaProjects\\GitHub\\Java\\src\\com\\tutorial\\projects\\strings\\test.txt"
-            ));
-
-            List<Sentence> sentences = new ArrayList<>();
+            sentences = new ArrayList<>();
 
             String line;
-            String last = "";
+            //   String last = "";
             while ((line = reader.readLine()) != null) {
                 String[] splitSent = line.split(SENTENСE_REGEX);
 
-                for (int i = 0; i < splitSent.length - 1; i++) {
+                for (int i = 0; i < splitSent.length; i++) {
                     sentences.add(new Sentence(splitSent[i]));
                 }
             }
 
+//            for (Sentence sentence : sentences) {
+//                System.out.println("Sentense: " + sentence);
+//            }
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+        for (Sentence s : sentences) {
+            System.out.println(s);
+        }
 
-            //           last = splitSent[splitSent.length - 1];
 
-            reader.close();
+        //           last = splitSent[splitSent.length - 1];
 
-            for (Sentence s : sentences) {
-                System.out.println(s);
-            }
-            // WORDS
 
-            BufferedReader readerWord = new BufferedReader(new FileReader(
+        //             WORDS
+        List<Word> words = null;
+        BufferedReader readerWord = null;
+
+        try {
+            readerWord = new BufferedReader(new FileReader(
                     "D:\\JavaProjects\\GitHub\\Java\\src\\com\\tutorial\\projects\\strings\\test.txt"
             ));
             String word;
-            List<Word> words = new ArrayList<>();
+            words = new ArrayList<>();
             while ((word = readerWord.readLine()) != null) {
                 String[] splitWord = word.split(WORD_REGEX);
-                for (int i = 0; i < splitWord.length - 1; i++) {
+                for (int i = 0; i < splitWord.length; i++) {
                     words.add(new Word(splitWord[i]));
                 }
             }
-            readerWord.close();
-            for (Word w : words) {
-                System.out.println(w);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                readerWord.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            // Punctuation marks
+        }
 
-            int sum = 0;
-            String sign = "";
-            char c;
-            //  Pattern pattern = Pattern.compile("[.?!—,]");
-            //   Matcher matcher = Pattern.compile(("[.?!—,]")).matcher();
-            BufferedReader readerSign = new BufferedReader(new FileReader(
+
+        for (Word w : words) {
+            System.out.println(w);
+        }
+
+//             Punctuation marks
+
+        int sum = 0;
+        String sign = "";
+        char c;
+        Pattern pattern = Pattern.compile("[.?!—,]");
+        Matcher matcher = Pattern.compile(("[.?!—,]")).matcher(WORD_REGEX);
+        BufferedReader readerSign = null;
+        try {
+            readerSign = new BufferedReader(new FileReader(
                     "D:\\JavaProjects\\GitHub\\Java\\src\\com\\tutorial\\projects\\strings\\test.txt"
             ));
-            List<PunctuationMark> signs = new ArrayList<>();
-//            while (matcher.find())
-//                System.out.println(matcher.group());
-            while ((sign = readerSign.readLine()) != null) {
-                for (int i = 0; i < sign.length(); i++) {
-                    switch (sign.charAt(i)) {
-                        case '.':
-                            sum++;
-                            break;
-                        case ',':
-                            sum++;
-                            break;
-                        case '?':
-                            sum++;
-                            break;
-                        case '!':
-                            sum++;
-                            break;
-                        case '—':
-                            sum++;
-                            break;
-                    }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        List<PunctuationMark> signs = new ArrayList<>();
+        while (matcher.find())
+         //   System.out.println(matcher.group());
+        while ((sign = readerSign.readLine()) != null) {
+            for (int i = 0; i < sign.length(); i++) {
+                switch (sign.charAt(i)) {
+                    case '.':
+                        sum++;
+                        break;
+                    case ',':
+                        sum++;
+                        break;
+                    case '?':
+                        sum++;
+                        break;
+                    case '!':
+                        sum++;
+                        break;
+                    case ';':
+                        sum++;
+                        break;
+                    case '-':
+                        sum++;
+                        break;
                 }
-
             }
-            readerSign.close();
+
+        }
+        readerSign.close();
 
             //SYMBOL
             BufferedReader readerSymbol = new BufferedReader(new FileReader(
                     "D:\\JavaProjects\\GitHub\\Java\\src\\com\\tutorial\\projects\\strings\\test.txt"
             ));
+            String line = "";
             List<Symbol> symbols = new ArrayList<>();
             int total = 0;
             while ((line = readerSymbol.readLine()) != null) {
@@ -113,22 +144,23 @@ public class Demo {
                 System.out.println(s);
             }
 
-            // Find word in sentence
-
-
-            System.out.println("Number of sentences: " + sentences.size());
-            System.out.println("Number of words: " + words.size());
-            System.out.println("Numbers of punctuation marks: " + sum);
-            System.out.println("Number of symbol: " + total);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException io) {
-            io.printStackTrace();
-        }
+//            // Find word in sentence
+//
+//
+        System.out.println("Number of sentences: " + sentences.size());
+        System.out.println("Number of words: " + words.size());
+        System.out.println("Numbers of punctuation marks: " + sum);
+        System.out.println("Number of symbol: " + total);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException io) {
+//            io.printStackTrace();
+//        }
         try {
-            FindWord.compareTo("тщ");
+            FindWord.compareTo("even");
         } catch (IOException e) {
             e.printStackTrace();
         }
+//    }
     }
 }
--- src/com/tutorial/projects/strings/Sentence.java
+++ src/com/tutorial/projects/strings/Sentence.java
@@ -1,15 +1,15 @@
 package com.tutorial.projects.strings;
 
 public class Sentence {
+
     private String s;
 
     public Sentence(String sentence) {
-        this.s = sentence.replaceAll("[,|.|?|!|\\n—|\\n\\n]", ""); //[.|?|!|—|\n\n]
+        this.s = sentence;//sentence.replaceAll("[…|:|,|.|?|!|\\n—|\\n\\n]", ""); //[.|?|!|—|\n\n]
     }
 
     @Override
     public String toString() {
         return s;
-
     }
 }
